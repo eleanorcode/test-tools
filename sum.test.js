cf50f124739bd9e51b72fb18bd37ad750bad4a60
ellukian <innocence17@gmail.com>
2021-08-10 17:49:45 +0300

    install and configure ESLint

--- sum.test.js
+++ sum.test.js
@@ -1,7 +1,7 @@
 import { sum } from './sum';
 
-describe ('sum', () => {
+describe('sum', () => {
   it('returns 2 for 3 and -1', () => {
-    expect(sum (3, -1)).toBe(2)
-  })
-})
+    expect(sum(3, -1)).toBe(2);
+  });
+});
